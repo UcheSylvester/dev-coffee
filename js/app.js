7fc55382f24c56eeb2731aa40d6c96db9b27d355
UcheSylvester <okorocode@gmail.com>
2020-03-01 12:39:45 +0100

    installed serivce worker and cached assets

--- js/app.js
+++ js/app.js
@@ -30,3 +30,12 @@ const showCoffees = () => {
 };
 
 document.addEventListener("DOMContentLoaded", showCoffees);
+
+if (navigator.serviceWorker) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(regEvent => console.log("Service worker registered!"))
+      .catch(err => console.log("Service worker not registered"));
+  });
+}
